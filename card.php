9ee9b9a19ac6105a9b9a42310d20d553b993855f
Mateus Cayres <mateuscayres3@gmail.com>
2026-02-23 17:09:18 -0400

    adiciona propriedade de admin no componente de card

--- card.php
+++ card.php
@@ -1,5 +1,6 @@
 <?php
-function renderCardItem($nome, $descricao, $local) {
+function renderCardItem($nome, $descricao, $local, $admin) {
+  if($admin){
   echo '
   <div class="col">
     <div class="card">
@@ -7,9 +8,23 @@ function renderCardItem($nome, $descricao, $local) {
         <h5 class="card-title">' . htmlspecialchars($nome) . '</h5>
         <p class="card-text">' . htmlspecialchars($descricao) . '</p>
         <p class="card-text">' . htmlspecialchars($local) . '</p>
+        <button class="btn btn-primary">Atualizar</button>
+        <button class="btn btn-danger">Remover</button>
       </div>
     </div>
   </div>';
+  }else{
+    echo '
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">' . htmlspecialchars($nome) . '</h5>
+          <p class="card-text">' . htmlspecialchars($descricao) . '</p>
+          <p class="card-text">' . htmlspecialchars($local) . '</p>
+        </div>
+      </div>
+    </div>';
+  }
 }
 ?>
 
