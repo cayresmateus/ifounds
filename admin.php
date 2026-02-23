292cbf58f5cb492facd7c6ab8d8d9dfc1937c1bd
FnxLucas <lucasvrxavier@gmail.com>
2026-02-23 11:41:44 -0400

    admin.html substituido pelo admin.php; footer retirado do index

--- admin.php
+++ admin.php
@@ -1,3 +1,7 @@
+<?php
+  require "card.php";
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -15,8 +19,15 @@
     <a class="titulo">
       <img class="w-25 p-2" src="./img/Ifounds_logo.png" alt="Logo IFOUNDS"> <p> - Administração</p> 
     </a>
-    <a class="btn btn-primary" href="index.html" role="button">Sair Pagina Admin</a>
+    <a class="btn btn-primary" href="index.php" role="button">Sair Pagina Admin</a>
   </header>
+
+  <main class="row row-cols-1 row-cols-md-2 g-4 p-2">
+    <?php renderCardItem("Nome do item perdido", "Descrição do item","Aonde foi encontrado") ?>
+    <?php renderCardItem("Nome do item perdido", "Descrição do item","Aonde foi encontrado") ?>
+    <?php renderCardItem("Nome do item perdido", "Descrição do item","Aonde foi encontrado") ?>
+    <?php renderCardItem("Nome do item perdido", "Descrição do item","Aonde foi encontrado") ?>
+  </main>
 </body>
 
 </html>
