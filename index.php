4151cf52d50383aad14e79727eddbb080442ccc2
Mateus Cayres <mateuscayres3@gmail.com>
2026-03-03 11:58:56 -0400

    componente para offcanvas criado

--- index.php
+++ index.php
@@ -31,32 +31,7 @@
       Cadastrar
     </button>
 
-    <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop"
-      aria-labelledby="staticBackdropLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="staticBackdropLabel">
-          Cadastro de item
-        </h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <form action="">
-          <div class="mb-3">
-            <label for="itemNome" class="form-label">Nome do item</label>
-            <input type="text" class="form-control" id="itemNome" required />
-          </div>
-          <div class="mb-3">
-            <label for="itemLocal" class="form-label">Aonde foi encontrado</label>
-            <input type="text" class="form-control" id="itemLocal" required />
-          </div>
-          <div class="mb-3">
-            <label for="itemDescricao" class="form-label">Descrição</label>
-            <textarea class="form-control" id="itemDescricao" rows="3" required></textarea>
-          </div>
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
-        </form>
-      </div>
-    </div>
+    <?php include '../IFounds/src/components/offcanvas.php'; ?>
   </header>
 
   <main class="row row-cols-1 row-cols-md-2 g-4 p-2">
