1056c866772d642641307831f727202d47655bc1
mateus cayres <mateuscayres3@gmail.com>
2026-03-30 17:02:48 -0400

    adiciona a classe AdminPageController para gerenciar a lógica da página administrativa

---
+++ src/controller/AdminPageController.php
@@ -0,0 +1,17 @@
+<?php
+namespace ifounds\controller;
+require __DIR__ . "/../../vendor/autoload.php";
+use ifounds\repositorio\ItemRepositorio;
+use ifounds\ConexaoDB;
+
+class AdminPageController {
+  public static function index() {
+    $isAdmin = true;
+    $conexaoDB = new ConexaoDB();
+    $pdo = $conexaoDB->conexao();
+    $itemRepositorio = new ItemRepositorio($pdo);
+    $items = $itemRepositorio->selectItens();
+
+    include __DIR__ . "/../../admin.php";
+  }
+}
